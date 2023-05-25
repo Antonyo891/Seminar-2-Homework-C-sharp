135895036bbf8417b118fceb136d9702bf2af1a0
Anton <B_atonyo_S@bk.ru>
2023-05-25 20:04:26 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+System.Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+int Div = Num, count = 1, index = 0;
+int[] array = new int[10];
+while (Div/10>0||Div/10<0)
+{
+    array[index] = Div%10;
+    Div=Div/10;
+    count = count + 1;
+    index++;    
+}
+if (count<3)
+System.Console.WriteLine($"В числе {Num} третьей цифры нет");
+else
+System.Console.WriteLine($"3 слева цифра в числе {Num} -> {array[index-2]}");
