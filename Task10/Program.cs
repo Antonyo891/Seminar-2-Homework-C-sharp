1fed1bf8a7d974758d309d3f0b49aae8d24a21ca
Anton <B_atonyo_S@bk.ru>
2023-05-25 19:17:57 +0300

    Task10 completed

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+System.Console.WriteLine("Введите трехзначное число");
+int Number = int.Parse(Console.ReadLine());
+int Num1 = Number/100, Num3= Number%10, 
+Num2 = (Number-100*Num1-Num3)/10; 
+Console.WriteLine(Num2);
